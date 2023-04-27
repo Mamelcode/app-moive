a826f4a6350af7e16d05e9e3b6fcfb4b892f244a
unknown <dondadam76@gmail.com>
2023-04-27 15:33:41 +0900

    feat/data maaper추가

--- src/main/java/data/show/ShowResults.java
+++ src/main/java/data/show/ShowResults.java
@@ -4,12 +4,14 @@ package data.show;
 //상영중인 영화 리스트
 public class ShowResults {
 
-	ShowResult[] showResults;
+	ShowResult[] results;
 
-	public ShowResult[] getShowResults() {
-		return showResults;
+	public ShowResult[] getResults() {
+		return results;
 	}
 
-	
-	
+	public void setResults(ShowResult[] results) {
+		this.results = results;
+	}
+
 }
