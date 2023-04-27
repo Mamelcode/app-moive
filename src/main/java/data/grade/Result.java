46863f730f1ae5e6dec1b0ea759ff0fc68c68a90
anwnlro <anwnlro@gmail.com>
2023-04-27 12:02:10 +0900

    feat/view : 예시추가

--- src/main/java/data/grade/Result.java
+++ src/main/java/data/grade/Result.java
@@ -5,22 +5,32 @@ package data.grade;
 public class Result {
 	
 	String id;   // 영화 아이디
-	String posterPath;   // 포스터
+	String poster_path;   // 포스터
 	String overview;  // 줄거리
 	String title; // 제목
-	
 	
 	public String getId() {
 		return id;
 	}
-	public String getPosterPath() {
-		return posterPath;
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPoster_path() {
+		return poster_path;
+	}
+	public void setPoster_path(String poster_path) {
+		this.poster_path = poster_path;
 	}
 	public String getOverview() {
 		return overview;
 	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
 	public String getTitle() {
 		return title;
 	}
-	
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
